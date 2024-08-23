cc8822d1db064962b51c8de2d328c9af42076ace
Mateus Yoon <mateusyoon0605@hotmail.com>
2024-08-23 16:06:34 -0300

    ultimos ajustes

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function toggleMode(){
+function toggleMode() {
   const html = document.documentElement // documentElement seria o HTML do codigo
-  html.classList.toggle('light')
+  html.classList.toggle("light")
   //if(html.classList.contains("light")){
   //  html.classList.remove("light")
-  //} 
+  //}
   //else{
   //  html.classList.add("light")
   //}
-  
+
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  if(html.classList.contains("light")){ // Se estiver no modo light
+  if (html.classList.contains("light")) {
+    // Se estiver no modo light
     // Adiciona imagem do modo light
-    img.setAttribute('src', './assets/parque-mirante.jfif')
-  }
-  else { // Else se nao estiver no modo light
-    img.setAttribute('src', './assets/parque-mirante2.png')
+    img.setAttribute("src", "./assets/parque-mirante.jfif")
+  } else {
+    // Else se nao estiver no modo light
+    img.setAttribute("src", "./assets/parque-mirante2.png")
   }
 }
-
